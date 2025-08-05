ca6c06a3df88cf6e741b81b0fc1a27d2a3b878aa
hiendbui <hiendbui0@gmail.com>
2025-08-05 19:20:26 -0400

    Fix env variable

--- lib/db/seed.ts
+++ lib/db/seed.ts
@@ -5,7 +5,7 @@ import { createClient } from '@supabase/supabase-js';
 // biome-ignore lint: Forbidden non-null assertion.
 const SUPABASE_URL = process.env.SUPABASE_URL!;
 // biome-ignore lint: Forbidden non-null assertion.
-const SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY!;
+const SUPABASE_SERVICE_ROLE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_SERVICE_ROLE_KEY);
 
